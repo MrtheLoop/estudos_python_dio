410e5e303a349628a2e4c65041e34fd9a08bae32
MrtheLoop <quersatos2.s@gmail.com>
2023-05-09 07:58:08 -0300

    Atualizando aulas por ordem numérica para ter sequência a seguir

---
+++ Fundamentos em Python/1-constantes_variaveis.py
@@ -0,0 +1,24 @@
+nome = 'Matheus'
+idade = 21
+
+nome = 'Giovanna'
+
+print(nome, idade)
+
+#Ao alterar a variável nome de Matheus para Giovanna, o valor dela altera sem retorno do valor anterior
+
+#Posso também definir as variáveis tudo na mesma linha
+ 
+nome, idade = 'Matheus', 21
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+#devo utilizar este método de snake_case sempre, para melhor legibilidade e compreensão
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'SC', 'RS']
+
+#Quando tenho uma variável que não deve ser alterada (constante), devo colocar ela toda em maíusculo, assim subentende-se que se trata de uma constante
+
+print(BRAZILIAN_STATES)
