9fc95b92ed7926852206b831aed72b876fc47251
MrtheLoop <quersatos2.s@gmail.com>
2023-05-16 16:40:13 -0300

    Restaurando pastas apagadas equivocadamente e apagando pasta que realmente deveria ter sido apagada

--- Fundamentos em Python/3-Manipulacao_de_strings_com_Python/realizando_desafio_mes.py
+++
@@ -1,7 +0,0 @@
-month = input()
-
-months_dict = {"1":"January", "2":"february", "3":"march", "4":"april", "5":"may", "6":"june", "7":"july", "8":"august", "9":"september", "10":"october", "11":"november", "12":"december"}
-
-mes = months_dict[month]
-
-print(f"{mes}")
