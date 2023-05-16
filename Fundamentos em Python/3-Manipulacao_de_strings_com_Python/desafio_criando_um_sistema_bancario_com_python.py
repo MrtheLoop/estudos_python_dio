67078554ce1122dcb55d8dbf6e79e0392e3958ca
MrtheLoop <quersatos2.s@gmail.com>
2023-05-16 08:14:07 -0300

    adicionando arquivo do desafio (meu) e resolução do desafio (professor) para melhor entender e estudar a solução

--- Fundamentos em Python/3-Manipulacao_de_strings_com_Python/desafio_criando_um_sistema_bancario_com_python.py
+++ Fundamentos em Python/3-Manipulacao_de_strings_com_Python/desafio_criando_um_sistema_bancario_com_python.py
@@ -1,56 +1,27 @@
-menu_principal = """
-========= MENU ==========
-[1] Depositar
-[2] Sacar
-[3] Extrato
-[9] Sair
+menu = '''
+======MENU=======
 
-Digite uma opção: 
-"""
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
 
-menu_pos_operacao = """
+'''
+saldo = 0
+limite = 500
+extrato = ''
+numero_saques = 0
+LIMITE_SAQUES = 3
 
-======== MENU PÓS OPERAÇÃO ========
-    Você quer fazer outra operação?
-
-[S] Sim
-[N] Não
-
-:
-"""
-
-saldo = 2000.00
-quantidade_saque = 0
-LIMITE_SAQUE = 3
-opcao = -1
-opcao1 = -1
-
-while opcao != 0:
-
-    opcao = int(input(menu_principal))
-
-    if opcao == 1:
-        valor_depositado = int(input("Insira o valor que deseja depositar: "))
-        print("Calculando valor depoistado...")
-        print(f"Valor deposito com sucesso. Seu saldo atual é de R${saldo + valor_depositado}")
-        opcao1 = (input(menu_pos_operacao))
-
-        if opcao1 =="S":
-            print(opcao)
-
-        elif opcao1 == "N":
-            break
-
-        else:
-            print("Opção inválida! Verifique o menu e tente novamente.")
-            break
-
-
-    if opcao == 2:
-        valor = int(input("Digite o valor que deseja sacar: "))
-        print(f"Saque realizado. Seu saldo agora é de {saldo - valor}")
-        print(opcao1)
-        
-
-else:
-    print("Opção inválida! Verifique o menu e tente")
+while True:
+    opcao = input (menu)
+    if opcao == "d":
+        print("Depósito")
+              
+    elif opcao == "s":
+        print("Saque")
+              
+    elif opcao == "e":
+        print("Extrato")
+    elif opcao == "q":
+     break
